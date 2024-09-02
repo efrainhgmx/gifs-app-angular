ffebf76bcffa7321d17d170c82fa32b257bb81df
Efrain Hernandez <efrainhg2050@gmail.com>
2024-09-02 16:09:09 -0600

    Card list component

---
+++ src/app/gifs/components/card-list/card-list.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'gifs-card-list',
+  template: `
+   <ul class="list-group">
+            <li class="list-group-item">1</li>
+  </ul>`
+})
+export class CardListComponent {
+
+}
