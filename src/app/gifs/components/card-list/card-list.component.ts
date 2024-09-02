b475ea7dad938eb37d15d0eb5b23464bfcc81e92
Efrain Hernandez <efrainhg2050@gmail.com>
2024-09-02 16:16:54 -0600

    Card list structure

--- src/app/gifs/components/card-list/card-list.component.ts
+++ src/app/gifs/components/card-list/card-list.component.ts
@@ -2,10 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'gifs-card-list',
-  template: `
-   <ul class="list-group">
-            <li class="list-group-item">1</li>
-  </ul>`
+  templateUrl: './card-list.component.html' 
 })
 export class CardListComponent {
 
