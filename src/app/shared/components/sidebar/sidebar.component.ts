0d3e0ad2456f8c130ffabf0b2e3104eca87d7272
Efrain Hernandez <efrainhg2050@gmail.com>
2024-09-02 14:05:58 -0600

    Sidebar component

---
+++ src/app/shared/components/sidebar/sidebar.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'shared-sidebar',
+    templateUrl: './sidebar.component.html',
+    styleUrl: './sidebar.component.css'
+})
+
+export class SidebarComponent implements OnInit {
+    constructor() { }
+
+    ngOnInit() { }
+}
